a51df4477434a28912c43acaae1bd16096554c2b
Shimon <shimonbhuiya02@gmail.com>
2025-09-01 18:34:10 +0600

    Navigation Fragment

---
+++ app/src/main/java/com/bcsbattle/weatherapp/UI/Location/LocationFragment.kt
@@ -0,0 +1,26 @@
+package com.bcsbattle.weatherapp.UI.Location
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bcsbattle.weatherapp.R
+import com.bcsbattle.weatherapp.databinding.ActivityMainBinding
+
+class LocationFragment : Fragment() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        return binding.root
+
+
+    }
+
+
+}
